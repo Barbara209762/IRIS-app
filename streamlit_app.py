9d13185a13c5e7a00ab0df3c0dafd258dd54c8d7
Barbara209762 <massoubarbara55@gmail.com>
2024-11-19 13:34:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,27 @@ chart = alt.Chart(data).mark_point().encode( x='SepalLength', y='PetalLength')
 st.altair_chart(chart, use_container_width=True)
 
 
+import streamlit as st
+import numpy as np
+import matplotlib.pylab as plt
+
+st.title("Simulation[tm]")
+st.write("Here is our super important simulation")
+
+st.sidebar.markdown("## Controls")
+st.sidebar.markdown("You can *change* the values to change the chart.")
+x = st.sidebar.slider('Slope', min_value=0.01, max_value=0.10, step=0.01)
+y = st.sidebar.slider('Noise', min_value=0.01, max_value=0.10, step=0.01)
+
+st.write(f"x={x} y={y}")
+values = np.cumprod(1 + np.random.normal(x, y, (100, 10)), axis=0)
+
+for i in range(values.shape[1]):
+    plt.plot(values[:, i])
+
+st.pyplot()
+
+
+
 
 
