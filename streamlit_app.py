c5db2379af041a751350e227876a07b0184e97ab
Barbara209762 <massoubarbara55@gmail.com>
2024-11-19 13:45:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ st.sidebar.title("Sidebar")
 # Add widgets to the sidebar
 option = st.sidebar.selectbox(
     "Select an option:",
-    ["Option 1", "Option 2", "Option 3"]
+    ["Option iris", "Option data", "Option github"]
 )
 
 # Display selected option
