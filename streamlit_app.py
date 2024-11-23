12d8ab39690e15cfe0576fc952d9aaeba38e1595
Barbara209762 <massoubarbara55@gmail.com>
2024-11-23 20:50:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 #pip show seaborn
+source /path/to/your/venv/bin/activate
+import seaborn as sns
+print("Seaborn importé avec succès!")
+
+#pip install --upgrade seaborn pandas matplotlib
