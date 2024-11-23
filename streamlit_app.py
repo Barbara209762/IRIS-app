abc23736530ef86080288f088f0c8330fcf9af0f
Barbara209762 <massoubarbara55@gmail.com>
2024-11-23 19:44:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd 
 import altair as alt
 #import matplotlib.pyplot as plt
-data - pd.read_csv("Iris.csv", delimiter - ';')
+data = pd.read_csv("Iris.csv", delimiter - ';')
 st.title("Mon premier dashboard avec Streamlit")
 #st. table(data)
 # Créer un chart Altair
@@ -12,7 +12,7 @@ chart = alt.Chart(data) -mark_bar() -encode( x='SepalLength', y='SepalWidth' )
 st.altair_chart(chart, use_container_width=True)
 chart = alt. Chart(data) mark_point() -encode( x='SepalLength', y='PetalLength')
 # Afficher le chart sur Streamlit
-st.altair_chart(chart, use_container_width-True)
+st.altair_chart(chart, use_container_width=True)
 
 
 
