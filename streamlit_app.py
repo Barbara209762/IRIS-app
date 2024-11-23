12dc1c055fd0dc3cf8c2f6bf4660cffd0412007c
Barbara209762 <massoubarbara55@gmail.com>
2024-11-23 14:26:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,13 @@ import altair as alt
 data=pd.read_csv("Iris.csv", delimiter = ';')
 st.title("Mon premier dashboard avec Streamlit")
 #st. table(data)
-# Créer un chart Altair
-chart - alt.Chart(data) -mark_bar() -encode( x='SepalLength', y='SepalWidth' )
+chart = alt.Chart(data).mark_bar().encode(
+    alt.X('SepalLength:Q', bin=True),  # Specify 'SepalLength' as quantitative and bin it
+    alt.Y('count()') # Count the number of occurrences in each bin
+    # y='SepalWidth'  <- Remove this or it might cause an error. Bar chart with x='SepalLength', y='SepalWidth' is unusual
+) 
+
+chart
 # Afficher le chart sur Streamlit
 st.altair_chart(chart, use_container_width=True)
 
