1b4f9de9c8941ec380e55090203ddf2cc38b7068
Barbara209762 <massoubarbara55@gmail.com>
2024-11-23 14:15:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,9 @@ st.title("Mon premier dashboard avec Streamlit")
 chart - alt.Chart(data) -mark_bar() -encode( x='SepalLength', y='SepalWidth' )
 # Afficher le chart sur Streamlit
 st.altair_chart(chart, use_container_width=True)
-chart - alt. Chart(data) mark_point() -encode( x='SepalLength', y='PetalLength')
+chart =alt. Chart(data) mark_point() -encode( x='SepalLength', y='PetalLength')
 # Afficher le chart sur Streamlit
-st.altair_chart(chart, use_container_width-True)
+st.altair_chart(chart, use_container_width=True)
 
 
  
